e0342ccbdde875663a9f84e761834dfead7f4664
tanjiancheng <562575412@qq.com>
2025-03-20 11:31:55 +0800

    修复在应用不退出执行attach连不上问题
    Signed-off-by: tanjiancheng <562575412@qq.com>

--- packages/flutter_tools/lib/src/ohos/ohos_device.dart
+++ packages/flutter_tools/lib/src/ohos/ohos_device.dart
@@ -649,10 +649,10 @@ class HdcLogReader extends DeviceLogReader {
 
     // If past logs are included then filter for 'flutter' logs only.
     if (includePastLogs) {
-      args.addAll(<String>['T','flutter']);
+      args.addAll(<String>['logcat', '|', 'grep', 'flutter']);
     } else {
       // if not includePastLogs, execute an hour log.
-      args.addAll(<String>['logcat','-d','-G','3600']);
+      args.addAll(<String>['logcat', '-d', '-G', '3600']);
     }
     final Process process =
         await processManager.start(device.hdcCommandForDevice(args));
