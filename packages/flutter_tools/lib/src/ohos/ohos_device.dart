21a74d31a7bb8012d92e4042b25ee6bbe7fb048f
tanjiancheng <562575412@qq.com>
2025-03-21 17:11:45 +0800

    修复在应用不退出执行attach连不上问题
    Signed-off-by: tanjiancheng <562575412@qq.com>

--- packages/flutter_tools/lib/src/ohos/ohos_device.dart
+++ packages/flutter_tools/lib/src/ohos/ohos_device.dart
@@ -650,7 +650,6 @@ class HdcLogReader extends DeviceLogReader {
     // If past logs are included then filter for 'flutter' logs only.
     if (includePastLogs) {
       args.addAll(<String>['-e', 'flutter']);
-
     } else {
       // execute timestamp log
       args.addAll(<String>['-v', 'time']);
