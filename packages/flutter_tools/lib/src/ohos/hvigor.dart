2035d0108ce9c0e8c0fb6c146a97e7000d5e80a3
liuyicheng3 <liuyicheng@mucang.cn>
2025-05-23 10:07:18 +0800

    flutter run --dart-define=OHOS_TARGET_MODULES=module1,module2

--- packages/flutter_tools/lib/src/ohos/hvigor.dart
+++ packages/flutter_tools/lib/src/ohos/hvigor.dart
@@ -687,18 +687,48 @@ class OhosHvigorBuilder implements OhosBuilder {
     Logger? logger,
   ) async {
     final String ohosProjectPath = project.ohos.ohosRoot.path;
-    final List<OhosModule> originModules = ohosBuildData.harModules;
-    // ohosBuildData.flutterPluginModuleNameList 如果为空的话  ，modules = originModules
-    List<OhosModule> modules;
+    final List<OhosModule> allHarModules = ohosBuildData.harModules;
+     // ohosBuildData.flutterPluginModuleNameList 如果为空的话  ，modules = originModules
+    List<OhosModule> originModules;
     if (ohosBuildData.flutterPluginModuleNameList.isEmpty) {
-      modules = originModules;
+      originModules = allHarModules;
     } else {
+      originModules = allHarModules
+          .where((OhosModule element) => ohosBuildData.flutterPluginModuleNameList.contains(element.name))
+          .toList();
+    }
+    if (originModules.isEmpty) {
+      return;
+    }
+
+    // 获取指定要构建的模块列表
+    final String? targetModules = ohosBuildInfo.buildInfo.dartDefines
+        .firstWhere((String element) => element.startsWith('OHOS_TARGET_MODULES='), orElse: () => '')
+        .replaceFirst('OHOS_TARGET_MODULES=', '');
+
+    // 筛选需要构建的模块
+    List<OhosModule> modules = originModules;
+    if (targetModules?.isNotEmpty == true) {
+      // 如果指定了要构建的模块,则只构建指定的模块
+      final List<String> moduleNames = targetModules!.split(',');
+      modules = originModules.where((OhosModule element) => moduleNames.contains(element.name)).toList();
+    } else if (ohosBuildData.flutterPluginModuleNameList.isNotEmpty) {
+      // 如果有插件模块列表,则构建插件模块
       modules = originModules
           .where((OhosModule element) => ohosBuildData.flutterPluginModuleNameList.contains(element.name))
           .toList();
     }
-    if (modules.isEmpty) {
-      return;
+
+    // 检查新增的 har 模块
+    final Directory harDir = globals.fs.directory(globals.fs.path.join(ohosRootPath, 'har'));
+    if (harDir.existsSync()) {
+      for (final OhosModule module in originModules) {
+        final File harFile = globals.fs.file(globals.fs.path.join(harDir.path, '${module.name}.har'));
+        if (!harFile.existsSync() && !modules.contains(module)) {
+          // 如果 har 目录下没有对应的 har 文件,说明是新增模块,需要构建
+          modules.add(module);
+        }
+      }
     }
 
     if (modules.isEmpty) {
