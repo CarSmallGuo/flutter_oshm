f808fbc4fce714a47632305cc6c092c3b21019ed
Kemin123 <luojianming4@h-partners.com>
2025-06-06 14:48:12 +0800

    新增ltpo
    Signed-off-by: Kemin123 <luojianming4@h-partners.com>

--- packages/flutter/lib/src/services/system_channels.dart
+++ packages/flutter/lib/src/services/system_channels.dart
@@ -561,4 +561,8 @@ abstract final class SystemChannels {
   static const MethodChannel keyboard = OptionalMethodChannel(
     'flutter/keyboard',
   );
+
+  static const MethodChannel nativeVsync = MethodChannel(
+    'flutter/nativevsync'
+  );
 }
