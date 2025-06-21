a53a933cda24c90a1ea4eac2b5b6d1630a5d0409
lihui868 <george.lihui@huawei.com>
2025-06-21 09:46:08 +0800

    add [--no-codesign] args when building hap/app
    Signed-off-by: lihui868 <george.lihui@huawei.com>

--- packages/flutter_tools/lib/src/commands/build_hap.dart
+++ packages/flutter_tools/lib/src/commands/build_hap.dart
@@ -41,6 +41,11 @@ class BuildHapCommand extends BuildSubCommand {
       allowed: <String>['ohos-arm64', 'ohos-arm', 'ohos-x64'],
       help: 'The target platform for which the app is compiled.',
     );
+
+    argParser.addFlag('codesign',
+      defaultsTo: true,
+      help: 'Codesign the application bundle (only available on device builds).',
+    );
   }
 
   @override
@@ -51,6 +56,8 @@ class BuildHapCommand extends BuildSubCommand {
 
   @override
   bool get reportNullSafety => false;
+
+  bool get shouldCodesign => boolArg('codesign');
 
   @override
   Future<Set<DevelopmentArtifact>> get requiredArtifacts async => <DevelopmentArtifact>{
@@ -67,6 +74,7 @@ class BuildHapCommand extends BuildSubCommand {
     final OhosBuildInfo ohosBuildInfo = OhosBuildInfo(
       buildInfo,
       targetArchs: stringsArg('target-platform').map<OhosArch>(getOhosArchForName),
+      shouldCodesign: shouldCodesign
     );
     await ohosBuilder?.buildHap(
       project: FlutterProject.current(),
