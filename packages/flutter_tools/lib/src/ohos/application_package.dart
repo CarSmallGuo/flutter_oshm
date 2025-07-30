9251df4e9cb8c031428e85c088bb199f4692d0f9
hezhengyi <ssyzesb@qq.com>
2025-07-30 19:16:55 +0800

    修复使用--flavor参数在生成har文件时的报错
    Signed-off-by: hezhengyi <ssyzesb@qq.com>

--- packages/flutter_tools/lib/src/ohos/application_package.dart
+++ packages/flutter_tools/lib/src/ohos/application_package.dart
@@ -71,8 +71,8 @@ class OhosHap extends ApplicationPackage implements PrebuiltApplicationPackage {
         }
       }
     }
-    for (final OhosModule element in ohosBuildData.moduleInfo.moduleList) {
-      element.flavor = flavor;
+    for (final OhosModule module in ohosBuildData.moduleInfo.moduleList) {
+      module.setFlavor(flavor);
     }
     return OhosHap(
         id: bundleName,
@@ -244,7 +244,6 @@ class OhosModule {
     required this.isEntry,
     required this.mainElement,
     required this.type,
-    required this.flavor,
   });
 
   final String name;
@@ -252,7 +251,14 @@ class OhosModule {
   final String? mainElement;
   final OhosModuleType type;
   final String srcPath;
-  String flavor;
+  String flavor = FLAVOR_DEFAULT;
+
+  void setFlavor(String? inputFlavor) {
+    // 设置ohos模块的flavor
+    final File buildProfile =
+        globals.fs.directory(srcPath).childFile(OhosProject.kBuildProfileName);
+    flavor = getFlavor(buildProfile, inputFlavor);
+  }
 
   static List<OhosModule> fromOhosProject(OhosProject ohosProject) {
     final Set<String> modulePathSet = <String>{};
@@ -297,7 +303,6 @@ class OhosModule {
 
   static OhosModule fromModulePath({
     required String modulePath,
-    String? flavor,
   }) {
     modulePath = globals.fs.path
         .normalize(globals.fs.file(modulePath).resolveSymbolicLinksSync());
@@ -323,7 +328,7 @@ class OhosModule {
           isEntry: isEntry,
           mainElement: isEntry ? module['mainElement'] as String : null,
           type: OhosModuleType.fromName(type),
-          flavor: flavor ?? FLAVOR_DEFAULT);
+      );
     } on Exception catch (e) {
       throwToolExit('parse module.json5 error , $moduleJsonPath . error: $e');
     }
