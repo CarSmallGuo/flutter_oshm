26894382a0efca884414ee44cf45fe68e4232e1a
g60090854 <gaoweiheng1@h-partners.com>
2025-08-16 12:20:29 +0800

    新增sdk获取npmPath和nodePath方法
    Signed-off-by: g60090854 <gaoweiheng1@h-partners.com>

--- packages/flutter_tools/lib/src/ohos/ohos_sdk.dart
+++ packages/flutter_tools/lib/src/ohos/ohos_sdk.dart
@@ -8,7 +8,7 @@ import 'dart:collection';
 
 import 'package:cli_config/cli_config.dart';
 import 'package:json5/json5.dart';
-
+import 'package:path/path.dart' as p;
 import '../base/file_system.dart';
 import '../base/platform.dart';
 import '../globals.dart' as globals;
@@ -39,6 +39,27 @@ String? _getHdcPath(String sdkPath) {
         return path;
       }
     }
+  }
+  return null;
+}
+
+// find npm in nodePath
+String? _getNpmPath(String? nodePath){
+  final bool isWindows = globals.platform.isWindows;
+  final tempPath, npmName = isWindows? 'npm.cmd' : 'npm';
+  if(nodePath != null){
+    tempPath = nodePath;
+  }else{
+    globals.printTrace('The current nodePath is empty');
+    return null;
+  }
+
+  String? path = globals.fs.path.join(tempPath, npmName);
+  if(globals.fs.file(path).existsSync()){
+    if(isWindows){ //若是Windows平台，则对路径进行格式化
+      path = p.windows.normalize(path);
+    }
+    return path;
   }
   return null;
 }
@@ -90,6 +111,9 @@ class OhosSdk implements HarmonySdk {
 
   @override
   String? get hdcPath => _getHdcPath(_sdkDir.path);
+
+  @override
+  String? get npmPath => _getNpmPath(this.getOhosSdkNodePath());
 
   @override
   List<String> get apiAvailable => getAvailableApi();
@@ -212,6 +236,19 @@ class OhosSdk implements HarmonySdk {
   String? getNdkLdPath({Platform? platform, Config? config}) {
     return _getNdkLdPath(sdkPath, platform: platform, config: config);
   }
+
+  @override
+  String? getOhosSdkNodePath(){
+    final String parentPath = globals.fs.path.dirname(sdkPath);
+    final String nodePath = globals.fs.path.join(parent, 'tools', 'node');
+    final Directory node = globals.fs.directory(nodePath);
+
+    if(node.existsSync()){
+      return nodePath;
+    }
+    return null;
+  }
+
 }
 
 class HmosSdk implements HarmonySdk {
@@ -224,6 +261,9 @@ class HmosSdk implements HarmonySdk {
 
   @override
   String? get hdcPath => _getHdcPath(_sdkDir.path);
+
+  @override
+  String? get npmPath => _getNpmPath(this.getNodePath());
 
   @override
   String get sdkPath => _sdkDir.path;
@@ -366,6 +406,18 @@ class HmosSdk implements HarmonySdk {
   @override
   String? getNdkLdPath({Platform? platform, Config? config}) {
      return _getNdkLdPath(sdkPath, platform: platform, config: config);
+  }
+
+  @override
+  String? getNodePath(){
+    final String parentPath = globals.fs.path.dirname(sdkPath);
+    final String nodePath = globals.fs.path.join(parent, 'tools', 'node');
+    final Directory node = globals.fs.directory(nodePath);
+
+    if(node.existsSync()){
+      return nodePath;
+    }
+    return null;
   }
 }
 
