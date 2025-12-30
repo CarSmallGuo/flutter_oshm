188447f79a8c6df58d100fd930671681b89b19f7
Kemin123 <541416002@qq.com>
2025-12-30 22:52:53 +0800

    LTPO的channel通信优化，速度值纠正
    Signed-off-by: Kemin123 <541416002@qq.com>

--- packages/flutter/lib/src/animation/animation_controller.dart
+++ packages/flutter/lib/src/animation/animation_controller.dart
@@ -12,7 +12,9 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/physics.dart';
 import 'package:flutter/scheduler.dart';
 import 'package:flutter/semantics.dart';
+import 'package:flutter/widgets.dart';
 
+import '../../material.dart';
 import 'animation.dart';
 import 'curves.dart';
 import 'listener_helpers.dart';
@@ -907,6 +909,19 @@ class AnimationController extends Animation<double>
     }
   }
 
+  /// This value represents whether the current animation interval is a ratio.
+  ///
+  /// If [upperBound] is not Infinite, and it less than 1.0,
+  /// consider it as an interval ratio.
+  bool get isIntervalRatio {
+    if (!upperBound.isInfinite &&
+        upperBound <= 1.0 &&
+        _simulation is _InterpolationSimulation) {
+      return true;
+    }
+    return false;
+  }
+
   void _tick(Duration elapsed) {
     _lastElapsedDuration = elapsed;
     final double elapsedInSeconds = elapsed.inMicroseconds.toDouble() / Duration.microsecondsPerSecond;
@@ -921,7 +936,7 @@ class AnimationController extends Animation<double>
 
     switch (defaultTargetPlatform) {
       case TargetPlatform.ohos:
-        SchedulerBinding.instance.sendTranslateVelocity(velocity);
+        WidgetsBinding.instance.sendTranslateVelocity(velocity, isIntervalRatio);
         break;
       case TargetPlatform.android:
       case TargetPlatform.iOS:
