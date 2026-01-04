515cec9a4f62666fb5b8b2f290549641d002cf58
谢代荣 <xiedairong@huawei.com>
2026-01-04 19:10:50 +0800

    配置CI门禁脚本
    Signed-off-by: 谢代荣 <xiedairong@huawei.com>

---
+++ ci/compile.sh
@@ -0,0 +1,199 @@
+# ci编译脚本
+# 编译命令为：sh ./third_party/flutter_flutter/ci/compile.sh
+
+ROOT_DIR=$(pwd)
+# 项目目录
+PROJECT_DIR="$ROOT_DIR/third_party"
+# 引擎目录
+ENGINE_DIR="$PROJECT_DIR/flutter_flutter/engine"
+# 归档目录
+ARCHIVE_DIR="$ROOT_DIR/Archive/out"
+# 编译模式，随机从debug、profile和release中选择一个
+MODES=("debug" "profile" "release")
+BUILD_MODE=${MODES[$RANDOM % ${#MODES[@]}]}
+
+# 目标分支
+TARGET_FLUTTER_BRANCH="oh-3.32.4-dev"
+
+# 同步项目依赖
+function gclient_sync() {
+    echo "同步项目依赖"
+    echo "$ cd $PROJECT_DIR/flutter_flutter"
+    cd $PROJECT_DIR/flutter_flutter
+    echo "同步 .gclient"
+    echo "$ cp -a ./ci/resources/. ."
+    cp -a ./ci/resources/. .
+    echo "$ ls -al"
+    ls -al
+
+    echo "$ gclient sync --ignore-dep-type=cipd -n"
+    gclient sync --ignore-dep-type=cipd -n
+    if [ $? -ne 0 ]; then
+        echo "Failed to execute: gclient sync --ignore-dep-type=cipd -n"
+        return 1
+    fi
+
+    echo "$ cipd ensure -root . -ensure-file cipd_manifest.txt"
+    cipd ensure -root . -ensure-file cipd_manifest.txt
+    if [ $? -ne 0 ]; then
+        echo "Failed to execute: cipd ensure -root . -ensure-file cipd_manifest.txt"
+        return 1
+    fi
+
+    # 临时，替换下载地址
+    sed -i 's|https://commondatastorage.googleapis.com|file:///home/tools/Flutter/repo/binary|g' $ENGINE_DIR/src/build/linux/sysroot_scripts/install-sysroot.py
+
+    echo "$ gclient runhooks"
+    gclient runhooks
+    if [ $? -ne 0 ]; then
+        echo "Failed to execute: gclient runhooks"
+        return 1
+    fi
+
+    # 3.7 以上跳过单元测试模块
+    if [ "$TARGET_FLUTTER_BRANCH" = "dev" ]; then
+        echo "无需跳过单元测试模块"
+        return 0
+    fi
+    sed -i 's|enable_unittests = current_toolchain == host_toolchain \|\| is_fuchsia \|\| is_mac|enable_unittests = false|g' $ENGINE_DIR/src/flutter/testing/testing.gni
+}
+
+# 编译 engine，随机从debug、profile和release中选择一个
+function compile_engine_random() {
+    echo "构建模式：$BUILD_MODE"
+    echo "$ cd $ENGINE_DIR"
+    cd $ENGINE_DIR
+
+    echo "开始编译engine"
+    echo "$ ./ohos -t $BUILD_MODE"
+    ./ohos -t $BUILD_MODE
+    if [ $? -ne 0 ]; then
+        echo "Failed to execute: ./ohos -t $BUILD_MODE"
+        return 1
+    fi
+
+    # 3.7 需要额外编译host
+    if [ "$TARGET_FLUTTER_BRANCH" = "dev" ]; then
+        echo "$ ./ohos -t $BUILD_MODE -n host"
+        ./ohos -t $BUILD_MODE -n host
+        if [ $? -ne 0 ]; then
+            echo "Failed to execute: ./ohos -t $BUILD_MODE -n host"
+            return 1
+        fi
+    fi
+
+    # 归档
+    (cp -a $ENGINE_DIR/src/out/. $ARCHIVE_DIR &)
+}
+
+# 编译 engine，全量
+function compile_engine_all() {
+    echo "编译 engine，全量"
+    echo "$ cd $ENGINE_DIR"
+    cd $ENGINE_DIR
+
+    echo "开始编译engine"
+    if [ "$TARGET_FLUTTER_BRANCH" = "dev" ]; then
+        # 3.7 需要额外编译host
+        echo "$ ./ohos && ./ohos -n host && ./ohos --ohos-cpu x64"
+        ./ohos && ./ohos -n host && ./ohos --ohos-cpu x64
+    else
+        echo "$ ./ohos && ./ohos --ohos-cpu x64"
+        ./ohos && ./ohos --ohos-cpu x64
+    fi
+    if [ $? -ne 0 ]; then
+        echo "engine 编译失败"
+        return 1
+    fi
+
+    # 归档
+    (cp -a $ENGINE_DIR/src/out/. $ARCHIVE_DIR &)
+}
+
+# 打包SDK
+function pack_flutter() {
+    echo "打包SDK"
+    echo "$ cd $PROJECT_DIR/flutter_flutter"
+    cd $PROJECT_DIR/flutter_flutter
+    echo "$ zip -r $ARCHIVE_DIR/flutter.ohos.zip *"
+    zip -r $ARCHIVE_DIR/sdk-$TARGET_FLUTTER_BRANCH.zip *
+}
+
+# 编译Tester
+function compile_tester() {
+    echo "编译Tester"
+    # 检查 flutter 环境
+    export PATH=$PROJECT_DIR/flutter_flutter/bin:$PATH
+    echo "$ echo \$PATH"
+    echo $PATH
+    echo "$ flutter doctor -v"
+    flutter doctor -v
+
+    echo "$ cd $PROJECT_DIR/flutter_tester"
+    cd $PROJECT_DIR/flutter_tester
+    if [ "$TARGET_FLUTTER_BRANCH" = "dev" ]; then
+        # 3.7版本不需要--local-engine-host
+        echo "$ flutter build hap --$BUILD_MODE --local-engine-src-path=$ENGINE_DIR/src --local-engine=ohos_${BUILD_MODE}_arm64"
+        flutter build hap --$BUILD_MODE --local-engine-src-path=$ENGINE_DIR/src --local-engine=ohos_${BUILD_MODE}_arm64
+    else
+        echo "$ flutter build hap --$BUILD_MODE --local-engine-src-path=$ENGINE_DIR/src --local-engine=ohos_${BUILD_MODE}_arm64 --local-engine-host=host_$BUILD_MODE"
+        flutter build hap --$BUILD_MODE --local-engine-src-path=$ENGINE_DIR/src --local-engine=ohos_${BUILD_MODE}_arm64 --local-engine-host=host_$BUILD_MODE
+    fi
+    # 归档
+    cp $PROJECT_DIR/flutter_tester/ohos/entry/build/default/outputs/default/entry-default-unsigned.hap $ARCHIVE_DIR/entry-default-unsigned.hap
+    if [ $? -ne 0 ]; then
+        echo "Failed to execute: flutter build hap --$BUILD_MODE"
+        return 1
+    fi
+}
+
+# 上传到obs
+function upload_to_obs() {
+    echo "上传到obs"
+    # 待完成
+}
+
+function compile() {
+    echo "开始编译"
+    pack_flutter
+    if [ $? -ne 0 ]; then
+        echo "Failed to execute: pack_flutter"
+        return 1
+    fi
+
+    gclient_sync
+    if [ $? -ne 0 ]; then
+        echo "Failed to execute: gclient_sync"
+        return 1
+    fi
+
+    if [ -z "${PR_URL}" ]; then
+        # PR_URL 为空，说明是每日构建，需要编译全量
+        echo "PR_URL 为空，说明是每日构建，需要编译全量"
+        compile_engine_all
+    else
+        # 门禁
+        compile_engine_random
+        echo "PR_URL 不为空，说明是门禁构建，需要编译随机"
+    fi
+    if [ $? -ne 0 ]; then
+        echo "engine 编译失败"
+        return 1
+    fi
+    
+    compile_tester
+    if [ $? -ne 0 ]; then
+        echo "Failed to execute: compile_tester"
+        return 1
+    fi
+    echo "编译阶段完成"
+}
+
+compile $@
+if [ $? -ne 0 ]; then
+    # 后台删除，src文件夹已被污染
+    (rm -rf $ENGINE_DIR/src &)
+    echo "编译阶段失败"
+    exit 1
+fi
+exit 0
---
+++ ci/prepare.sh
@@ -0,0 +1,191 @@
+# ci预编译脚本
+# 预编译命令为：sh ./third_party/flutter_flutter/ci/prepare.sh
+
+ROOT_DIR=$(pwd)
+# 项目目录
+PROJECT_DIR="$ROOT_DIR/third_party"
+# 引擎目录
+ENGINE_DIR="$PROJECT_DIR/flutter_flutter/engine"
+# 归档目录
+ARCHIVE_DIR="$ROOT_DIR/Archive/out"
+# 备份目录
+BACKUP_DIR="$ROOT_DIR/Backup"
+# repo 缓存仓库
+REPO_CACHE_DIR="/home/tools/Flutter/repo"
+# fluttertpc仓库列表
+FLUTTERTPC_REPOS=(
+  fluttertpc_angle
+  fluttertpc_boringssl_gen
+  fluttertpc_buildroot
+  fluttertpc_dart_native
+  fluttertpc_dart_sdk
+  fluttertpc_libcxx
+  fluttertpc_libcxxabi
+  fluttertpc_skia
+  fluttertpc_spirv-headers
+  fluttertpc_swiftshader
+  fluttertpc_vulkan-deps
+  fluttertpc_vulkan-headers
+  fluttertpc_zlib
+)
+
+# 目标分支
+TARGET_FLUTTER_BRANCH="oh-3.32.4-dev"
+TARGET_TESTER_BRANCH="main"
+
+# 检查环境
+function check_env() {
+    echo "检查环境"
+    echo "$ uname -a"
+    uname -a
+    echo "$ cat /etc/os-release"
+    cat /etc/os-release
+    echo "$ id -un"
+    id -un
+    echo "$ env"
+    env
+    # set
+    echo "检查工具"
+    echo "$ node -v"
+    node -v
+    echo "$ npm -v"
+    npm -v
+    echo "$ ohpm -v"
+    ohpm -v
+    echo "$ hvigorw -v"
+    hvigorw -v
+    echo "$ hdc -v"
+    hdc -v
+    echo "$ git --version"
+    git --version
+    echo "$ git config -l"
+    git config -l
+    echo "$ java -version"
+    java -version
+    echo "$ 检查网络"
+    echo "$ curl -s -o /dev/null -w \"%{http_code}\n\" -m 5 $PUB_HOSTED_URL"
+    curl -s -o /dev/null -w "%{http_code}\n" -m 5 $PUB_HOSTED_URL
+    echo "$ curl -s -o /dev/null -w \"%{http_code}\n\" -m 5 $FLUTTER_STORAGE_BASE_URL"
+    curl -s -o /dev/null -w "%{http_code}\n" -m 5 $FLUTTER_STORAGE_BASE_URL
+    echo "$ curl -s -o /dev/null -w \"%{http_code}\n\" -m 5 https://chrome-infra-packages.appspot.com/prpc/cipd.Repository/GetInstanceURL"
+    curl -s -o /dev/null -w "%{http_code}\n" -m 5 https://chrome-infra-packages.appspot.com/prpc/cipd.Repository/GetInstanceURL
+    echo "检查项目文件"
+    echo "$ pwd"
+    pwd
+    echo "$ cd $PROJECT_DIR"
+    cd $PROJECT_DIR
+    echo "$ ls -al"
+    ls -al
+    mkdir -p $ARCHIVE_DIR
+    mkdir -p $BACKUP_DIR
+}
+
+# 同步缓存
+function sync_cache() {
+    echo "同步缓存"
+    # 刷新CIPD缓存时间，避免缓存过期被删除
+    echo "$ find $CIPD_CACHE_DIR -type f -exec touch {} +"
+    find $CIPD_CACHE_DIR -type f -exec touch {} +
+    # 刷新FLUTTERTPC仓库
+    echo "$ cd $REPO_CACHE_DIR"
+    cd $REPO_CACHE_DIR
+    for repo in "${FLUTTERTPC_REPOS[@]}"; do
+        echo "Fetching in $repo"
+        (cd "$repo" && git fetch --all)
+    done
+    echo "$ repo forall -c 'git fetch --all'"
+    repo forall -c 'git fetch --all'
+    echo "$ 检查缓存"
+    echo "$ ls -al $REPO_CACHE_DIR"
+    ls -al $REPO_CACHE_DIR
+    echo "$ ls -1A \"$REPO_CACHE_DIR\" | wc -l"
+    ls -1A "$REPO_CACHE_DIR" | wc -l
+    echo "$ ls -al $CIPD_CACHE_DIR/instances"
+    ls -al $CIPD_CACHE_DIR/instances
+    echo "$ ls -1A \"$CIPD_CACHE_DIR/instances\" | wc -l"
+    ls -1A "$CIPD_CACHE_DIR/instances" | wc -l
+}
+
+# 恢复engine
+function maybe_restore_engine() {
+    if [ ! -d $ENGINE_DIR/src/flutter ]; then
+        cp -a $BACKUP_DIR/src $ENGINE_DIR
+    fi
+}
+
+# 准备SDK
+function prepare_flutter() {
+    echo "准备项目文件"
+    echo "变基"
+    echo "$ cd $PROJECT_DIR/flutter_flutter"
+    cd $PROJECT_DIR/flutter_flutter
+    echo "$ git fetch --all"
+    git fetch --all
+    echo "$ git branch -a"
+    git branch -a
+    echo "$ git rebase remotes/gitcode/$TARGET_FLUTTER_BRANCH"
+    git rebase remotes/gitcode/$TARGET_FLUTTER_BRANCH
+    if [ $? -ne 0 ]; then
+        echo "rebase failed!!!"
+        return 1
+    fi
+    git log -10 --pretty=format:"%h - %s"
+    git status
+    git diff
+
+    # 获取commitid
+    COMMIT_ID=$(git rev-parse HEAD)
+    echo "COMMIT_ID: $COMMIT_ID"
+    # 写入 commitid 文件
+    echo $COMMIT_ID > $ARCHIVE_DIR/engine.ohos.har.version
+    echo $COMMIT_ID > $ARCHIVE_DIR/engine.ohos.version
+
+    # 备份（gclient sync失败时可能会将src里面的flutter文件夹删除，或src文件夹被污染）
+    echo "$ rm -rf $BACKUP_DIR/src"
+    rm -rf $BACKUP_DIR/src
+    echo "$ cp -a $ENGINE_DIR/src $BACKUP_DIR/src"
+    cp -a $ENGINE_DIR/src $BACKUP_DIR/src
+}
+
+# 准备Tester
+function prepare_tester() {
+    echo "准备Tester"
+    echo "$ cd $PROJECT_DIR/flutter_tester"
+    cd $PROJECT_DIR/flutter_tester
+    if [ $? -ne 0 ]; then
+        echo "flutter_tester 不存在"
+        return 1
+    fi
+    echo "$ git branch -a"
+    git branch -a
+    echo "$ git checkout $TARGET_TESTER_BRANCH"
+    git checkout $TARGET_TESTER_BRANCH
+    echo "$ git reset --hard"
+    git reset --hard
+    echo "$ git pull --rebase"
+    git pull --rebase
+}
+
+# 入口
+function prepare() {
+    check_env
+    sync_cache
+    maybe_restore_engine
+
+    prepare_flutter
+    if [ $? -ne 0 ]; then
+        echo "Failed to execute: prepare_flutter"
+        return 1
+    fi
+
+    prepare_tester
+    if [ $? -ne 0 ]; then
+        echo "Failed to execute: prepare_tester"
+        return 1
+    fi
+
+    echo "准备阶段完成"
+}
+
+prepare $@
+exit $?
