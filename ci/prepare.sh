06e6a00fb22759567d1181cbb3af904cdc1aef8c
谢代荣 <xiedairong@huawei.com>
2026-01-06 15:57:08 +0800

    Copyright
    Signed-off-by: 谢代荣 <xiedairong@huawei.com>

--- ci/prepare.sh
+++ ci/prepare.sh
@@ -1,29 +1,53 @@
-# ci预编译脚本
-# 预编译命令为：sh ./third_party/flutter_flutter/ci/prepare.sh
+#! /bin/bash
+# Copyright (c) 2025 Huawei Device Co., Ltd. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE_HW file.
+#
+# USE IN CI
+# preCompile: sh ./third_party/flutter_flutter/ci/prepare.sh
 
 ROOT_DIR=$(pwd)
-# 项目目录
+# Project directory
 PROJECT_DIR="$ROOT_DIR/third_party"
-# 归档目录
+# Archive directory
 ARCHIVE_DIR="$ROOT_DIR/Archive/out"
 
-# 目标分支
+# Target branch
 TARGET_FLUTTER_BRANCH="dev"
 TARGET_TESTER_BRANCH="main"
 
-# 检查环境
+# Check environment
 function check_env() {
-    echo "检查环境"
+    echo "Checking environment"
     echo "$ uname -a"
     uname -a
     echo "$ cat /etc/os-release"
     cat /etc/os-release
     echo "$ id -un"
     id -un
+    # Set environment variables
+    # command-line-tools
+    export TOOL_HOME=/home/tools/command-line-tools
+    export DEVECO_SDK_HOME=$TOOL_HOME/sdk
+    export PATH=$DEVECO_SDK_HOME/default/openharmony/toolchains:$TOOL_HOME/ohpm/bin:$TOOL_HOME/hvigor/bin:$TOOL_HOME/tool/node/bin:$PATH
+    # Flutter
+    export PUB_CACHE=/home/tools/Flutter/PUB
+    export PUB_HOSTED_URL=https://pub.flutter-io.cn
+    export FLUTTER_STORAGE_BASE_URL=https://storage.flutter-io.cn
+    # Flutter gclient
+    export PATH=/home/tools/depot_tools:$PATH
+    export DEPOT_TOOLS_UPDATE=0
+    export GCLIENT_SUPPRESS_GIT_VERSION_WARNING=1
+    # Flutter cipd
+    export CIPD_CACHE_DIR=/home/tools/cipd_cache
+    export CIPD_HTTP_USER_AGENT_PREFIX="offline"
+    export CIPD_NO_SELF_UPDATE=true
+    # llvm
+    export PATH=$DEVECO_SDK_HOME/default/openharmony/native/llvm/bin:$PATH
     echo "$ env"
     env
     # set
-    echo "检查工具"
+    echo "Checking tools"
     echo "$ node -v"
     node -v
     echo "$ npm -v"
@@ -40,14 +64,14 @@ function check_env() {
     git config -l
     echo "$ java -version"
     java -version
-    echo "$ 检查网络"
+    echo "$ Checking network"
     echo "$ curl -s -o /dev/null -w \"%{http_code}\n\" -m 5 $PUB_HOSTED_URL"
     curl -s -o /dev/null -w "%{http_code}\n" -m 5 $PUB_HOSTED_URL
     echo "$ curl -s -o /dev/null -w \"%{http_code}\n\" -m 5 $FLUTTER_STORAGE_BASE_URL"
     curl -s -o /dev/null -w "%{http_code}\n" -m 5 $FLUTTER_STORAGE_BASE_URL
     echo "$ curl -s -o /dev/null -w \"%{http_code}\n\" -m 5 https://chrome-infra-packages.appspot.com/prpc/cipd.Repository/GetInstanceURL"
     curl -s -o /dev/null -w "%{http_code}\n" -m 5 https://chrome-infra-packages.appspot.com/prpc/cipd.Repository/GetInstanceURL
-    echo "检查项目文件"
+    echo "Checking project files"
     echo "$ pwd"
     pwd
     echo "$ cd $PROJECT_DIR"
@@ -57,10 +81,10 @@ function check_env() {
     mkdir -p $ARCHIVE_DIR
 }
 
-# 准备SDK
+# Prepare SDK
 function prepare_flutter() {
-    echo "准备项目文件"
-    echo "变基"
+    echo "Preparing project files"
+    echo "Rebasing"
     echo "$ cd $PROJECT_DIR/flutter_flutter"
     cd $PROJECT_DIR/flutter_flutter
     echo "$ git fetch --all"
@@ -78,13 +102,13 @@ function prepare_flutter() {
     git diff
 }
 
-# 准备Tester
+# Prepare Tester
 function prepare_tester() {
-    echo "准备Tester"
+    echo "Preparing Tester"
     echo "$ cd $PROJECT_DIR/flutter_tester"
     cd $PROJECT_DIR/flutter_tester
     if [ $? -ne 0 ]; then
-        echo "flutter_tester 不存在"
+        echo "flutter_tester does not exist"
         return 1
     fi
     echo "$ git branch -a"
@@ -97,7 +121,7 @@ function prepare_tester() {
     git pull --rebase
 }
 
-# 入口
+# Entry point
 function prepare() {
     check_env
 
@@ -113,7 +137,7 @@ function prepare() {
         return 1
     fi
 
-    echo "准备阶段完成"
+    echo "Preparation phase completed"
 }
 
 prepare $@
