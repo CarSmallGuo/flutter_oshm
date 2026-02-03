224f996e83db2cae6c695d4efc1c71c72220ca72
xiaolieask <760956257@qq.com>
2026-02-03 11:53:37 +0800

    需求：新增状态栏点击回顶
    Signed-off-by: xiaolieask <760956257@qq.com>

---
+++ packages/flutter/lib/src/widgets/statusBar.dart
@@ -0,0 +1,45 @@
+import 'dart:async';
+import 'package:flutter/services.dart';
+
+class ChannelMessageHandler {
+  static const MethodChannel _channel = MethodChannel('flutter/statusBarClick');
+
+  static bool isInit = false;
+  static DateTime? lastCallTime;
+
+  // 使用广播流控制器允许多个监听者
+  static final StreamController<dynamic> _streamController =
+      StreamController<dynamic>.broadcast();
+
+  // 初始化Channel处理器
+  static void init() {
+    if (isInit) {
+      return;
+    }
+    isInit = true;
+    _channel.setMethodCallHandler((call) async {
+      throttle(() {
+        _streamController.add({
+        'method': call.method,
+        'arguments': call.arguments,
+      });
+      }, const Duration(milliseconds: 1000));
+    });
+  }
+
+  static throttle(Function() callback, Duration duration) {
+    final now = DateTime.now();
+    if (lastCallTime == null || now.difference(lastCallTime!) >= duration) {
+      lastCallTime = now;
+      callback();
+    }
+  }
+
+  // 获取消息流供组件订阅
+  static Stream<dynamic> get messageStream => _streamController.stream;
+
+  // 关闭流控制器
+  static void dispose() {
+    _streamController.close();
+  }
+}
