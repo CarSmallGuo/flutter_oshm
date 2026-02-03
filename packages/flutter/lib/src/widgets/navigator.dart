3a5d2035904d2ba2d25e4850e8d586ecc0367613
Kemin123 <541416002@qq.com>
2026-02-03 16:37:07 +0800

    增加路由跳转和标签页切换的检测能力
    Signed-off-by: Kemin123 <541416002@qq.com>

--- packages/flutter/lib/src/widgets/navigator.dart
+++ packages/flutter/lib/src/widgets/navigator.dart
@@ -3218,6 +3218,13 @@ class _RouteEntry extends RouteTransitionRecord {
     route.install();
     assert(route.overlayEntries.isNotEmpty);
     if (currentState == _RouteLifecycle.push || currentState == _RouteLifecycle.pushReplace) {
+      switch (defaultTargetPlatform) {
+        case TargetPlatform.ohos:
+          ServicesBinding.instance.reportNavigatorActivity('push', 'start');
+        default:
+          break;
+      }
+
       final TickerFuture routeFuture = route.didPush();
       currentState = _RouteLifecycle.pushing;
       routeFuture.whenCompleteOrCancel(() {
@@ -3233,6 +3240,13 @@ class _RouteEntry extends RouteTransitionRecord {
             navigator._debugLocked = false;
             return true;
           }());
+
+          switch (defaultTargetPlatform) {
+            case TargetPlatform.ohos:
+              ServicesBinding.instance.reportNavigatorActivity('push', 'finish');
+            default:
+              break;
+          }
         }
       });
     } else {
@@ -4473,6 +4487,12 @@ class NavigatorState extends State<Navigator> with TickerProviderStateMixin, Res
           }
           assert(entry.currentState == _RouteLifecycle.popping);
           canRemoveOrAdd = true;
+          switch (defaultTargetPlatform) {
+            case TargetPlatform.ohos:
+              ServicesBinding.instance.reportNavigatorActivity('pop', 'start');
+            default:
+              break;
+          }
         case _RouteLifecycle.popping:
           // Will exit this state when animation completes.
           break;
@@ -5720,6 +5740,13 @@ class NavigatorState extends State<Navigator> with TickerProviderStateMixin, Res
     // finishes synchronously.
     if (!_flushingHistory) {
       _flushHistoryUpdates(rearrangeOverlay: false);
+    }
+
+    switch (defaultTargetPlatform) {
+      case TargetPlatform.ohos:
+        ServicesBinding.instance.reportNavigatorActivity('pop', 'finish');
+      default:
+        break;
     }
 
     assert(() {
